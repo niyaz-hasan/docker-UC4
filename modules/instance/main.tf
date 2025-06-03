04d6a1bd1233ba2ea748fb5c12eb011d073a442d
niyaz-hasan <60500585+niyaz-hasan@users.noreply.github.com>
2025-06-03 18:44:58 +0530

    added instance

--- modules/instance/main.tf
+++ modules/instance/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "this" {
   ami           = "ami-02457590d33d576c3"  # Replace with appropriate AMI
-  instance_type = "t3a.small"
+  instance_type = "t2.medium"
   subnet_id     = var.subnet_id
   vpc_security_group_ids = var.vpc_security_group_ids
   user_data     = var.user_data
