2d22572b869906c2156630bda03b3bb4edec74ba
niyaz-hasan <60500585+niyaz-hasan@users.noreply.github.com>
2025-06-04 13:15:27 +0530

    Update main.tf

--- modules/sg_group/main.tf
+++ modules/sg_group/main.tf
@@ -45,25 +45,7 @@ resource "aws_security_group" "ec2_sg" {
   }
 }
 
-resource "aws_security_group" "rds_sg" {
-  name        = "rds-sg"
-  description = "Allow MySQL from Web SG"
-  vpc_id      = var.vpc_id
 
-  ingress {
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    security_groups = [aws_security_group.ec2_sg.id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 
 
 output "alb_security_group_id" {
